f155003c89e1c09ae8f5576908812a51262ddd11
phucgari <phucgari@gmail.com>
2023-04-13 13:47:20 +0700

    first commit

---
+++ src/test/java/com/casestudymodule4/CaseStudyModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudymodule4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
