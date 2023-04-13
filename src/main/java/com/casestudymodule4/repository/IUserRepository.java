12a92ed0e4b234f6d97a2f4580fe6986b340a8fb
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-04-13 17:36:22 +0700

    Update IUserRepository, IUserService and UserServiceImpl

--- src/main/java/com/casestudymodule4/repository/IUserRepository.java
+++ src/main/java/com/casestudymodule4/repository/IUserRepository.java
@@ -1,4 +1,14 @@
 package com.casestudymodule4.repository;
 
-public interface IUserRepository {
+import com.casestudymodule4.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String user);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
