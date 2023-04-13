b8a24da058d593e4660e9921344a5965c73b3b4d
Tuong <aumanhtuong09122001@gmail.com>
2023-04-13 18:18:25 +0700

    tao repo va service cho home

--- src/main/java/com/casestudymodule4/repository/IHomeRepository.java
+++ src/main/java/com/casestudymodule4/repository/IHomeRepository.java
@@ -1,4 +1,11 @@
 package com.casestudymodule4.repository;
 
-public interface IHomeRepository {
+import com.casestudymodule4.model.home.Home;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IHomeRepository extends JpaRepository<Home, Long> {
 }
