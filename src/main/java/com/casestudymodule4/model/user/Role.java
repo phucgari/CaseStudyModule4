0f4a0cc88ca58afabb706b7311756913444035c2
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-04-13 16:16:25 +0700

    Update model User and Role

--- src/main/java/com/casestudymodule4/model/user/Role.java
+++ src/main/java/com/casestudymodule4/model/user/Role.java
@@ -1,4 +1,44 @@
 package com.casestudymodule4.model.user;
 
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    public enum RoleType {
+        HOST, USER
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleType name;
+    public Role() {
+    }
+
+    public Role(Long id, RoleType name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public RoleType getName() {
+        return name;
+    }
+
+    public void setName(RoleType name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/casestudymodule4/model/user/User.java
+++ src/main/java/com/casestudymodule4/model/user/User.java
@@ -1,6 +1,146 @@
 package com.casestudymodule4.model.user;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"})
+})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String avatar;
+
+    @NotBlank
+    @Size(min = 6, max = 50)
+    private String fullName;
+
+    @NotBlank
+    @Size(min = 6, max = 32)
+    private String username;
+
+    @JsonIgnore
+    @NotBlank
+    @Size(min = 6, max = 8)
+    private String password;
+
+    @NotBlank
+    @Size(max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 10, max = 15)
+    private String phone;
+
+    @NotBlank
+    private String address;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String avatar, String fullName, String username, String password, String email, String phone, String address, Set<Role> roles) {
+        this.id = id;
+        this.avatar = avatar;
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAvatar() {
+        if (avatar == null) {
+            return "";
+        }
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
+
