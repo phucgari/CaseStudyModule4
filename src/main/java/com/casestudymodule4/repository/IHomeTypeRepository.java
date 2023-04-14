b3874e0cb3547231d6499f4da666c40043791c6d
Tuong <aumanhtuong09122001@gmail.com>
2023-04-14 08:44:48 +0700

    tao phuong thuc cho home va picture

--- src/main/java/com/casestudymodule4/repository/IHomeTypeRepository.java
+++ src/main/java/com/casestudymodule4/repository/IHomeTypeRepository.java
@@ -1,10 +1,9 @@
 package com.casestudymodule4.repository;
 
-import com.casestudymodule4.model.home.Home;
+import com.casestudymodule4.model.home.type.HomeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface IHomeRepository extends JpaRepository<Home, Long> {
+public interface IHomeTypeRepository extends JpaRepository<HomeType, Long> {
 }
